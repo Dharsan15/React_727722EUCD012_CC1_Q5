6f5b84c1445ba9382c6d221895533ee028106e96
Dharsan15 <dharsanravi6677@gmail.com>
2023-11-21 16:34:32 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ButtonToggle from './ButtonToggle';
-import IncreaseSize from './IncreaseSize'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <IncreaseSize/>
+    <ButtonToggle/>
 );
 
 
