4d3eaa86a2d3782b3f8cc3f37737f0db1fdc4564
Dharsan15 <dharsanravi6677@gmail.com>
2023-11-22 09:23:50 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ButtonToggle from './ButtonToggle';
-
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ButtonToggle/>
+    <App/>
 );
 
 
